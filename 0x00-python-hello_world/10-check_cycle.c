8fd5ec023afaa3267af95a5782d9be1ca7e44664
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-11-27 13:03:32 -0800

    ./cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/*
+ * check_cycle - function to check the cycle of the list
+ * @list: pointer to the list created
+ * Return: (0) fail, (1) success
+int check_cycle(listint_t *list)
+{
+	listint_t *node = list;
+	listint_t *data = list;
+
+	while (node && data->next)
+	{
+		data = data->next;
+		node = data->next->next;
+		if (node == data)
+			return (1);
+	}
+	return (0);
+}
