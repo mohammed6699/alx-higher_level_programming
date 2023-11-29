9ca3320add00a18b3dca5ac477a262d79bff2cce
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-11-29 05:48:28 -0800

    ./6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for f in range(i, 10):
-        print("{}{}".format(i, f), end = "\n" if i == 0 and f == 9 else ", ")
+        if (i < f):
+            print("{}{}".format(i, f), end = "\n" if i == 8 and f == 9 else ", ")
