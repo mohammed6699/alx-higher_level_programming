564489d3411f8fca467f08ec555325bf36663821
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-12-04 09:08:31 -0800

    ./5-main.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    coun = " "
+    for i in range(len(my_string)):
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
+            coun += my_string[i]
+    return coun
