981581bdb35161f02c757d08f73efe223b4ac870
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-12-04 12:36:37 -0800

    9-main.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,5 @@
 #!/bin/usr/python3
 def max_integer(my_list=[]):
-    number = my_list[0]
-    for i in range(len(my_list)):
-        if (my_list[i] > number):
-            number = my_list[i]
-    return (number)
+    list_copy = my_list.copy()
+    list_copy.sort()
+    return (list_copy[-1])
