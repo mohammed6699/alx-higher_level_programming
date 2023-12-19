a8ca49b7ddaa9236cc4a52bc169eb81c024346df
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-12-19 14:48:35 -0800

    ./1.1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""Square module"""
+
 class Square:
+    """Define Square"""
+
     def __init__ (self, size):
+    """Constructor
+
+    Args:
+        size: length of a side of square
+        """
         self.__size = size
