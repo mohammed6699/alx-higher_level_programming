7cdeb257bdb0683fbaae1cf6785b520a7849b317
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-12-20 01:41:45 -0800

    ./2.2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,6 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 """square module"""
+
 
 class Square:
     """Define a square"""
@@ -17,6 +18,6 @@ class Square:
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if (size < 0):
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
