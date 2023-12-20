32fa7733adc694b1e0431d9e62241fefd9f63fef
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-12-20 12:22:13 -0800

    ./5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Square model"""
+
+class Square:
+    """Define a square"""
+
+    def __init__(self, size=0):
+        """Constructor
+        Args:
+            size: length of a side of square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """property to determine the size of square
+
+        Raises:
+            TypeError: if the size is not int
+            ValueError: if the size is < 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        The area of square
+        Return:
+            area of a square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                print('#', end="\n")
