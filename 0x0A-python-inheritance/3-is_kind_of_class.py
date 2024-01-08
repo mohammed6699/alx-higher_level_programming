ec16161efde0fd19ae02d918c74f08e11f2c942f
mohammed6699 <mohammedahmed.refaat@gmail.com>
2024-01-08 13:49:27 -0800

    ./3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Module for is_kind_of_class"""
+
+def is_kind_of_class(obj, a_class):
+    """function that check if the object isinstance for it or not"""
+    return isinstance(obj, a_class)
