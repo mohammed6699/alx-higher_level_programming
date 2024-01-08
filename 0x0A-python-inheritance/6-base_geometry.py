d09ff1a8167c2674229eb440b72b9fdcec876ed4
mohammed6699 <mohammedahmed.refaat@gmail.com>
2024-01-08 14:04:22 -0800

    ./6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for base_geometry"""
+
+class BaseGeometry:
+    """Super class BaseGeometry"""
+    def area(self):
+        """Public instance method that print an error masseage"""
+        raise Exception("area() is not implemented")
