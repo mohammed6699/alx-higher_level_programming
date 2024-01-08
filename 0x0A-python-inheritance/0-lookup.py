9575d08ee33e286839b5a06537345b3159ede4b8
mohammed6699 <mohammedahmed.refaat@gmail.com>
2024-01-08 11:36:13 -0800

    ./0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for Lookup method"""
+
+def lookup(obj):
+    """Looks up for list of objrct
+    Args:
+        obj: (object) for list
+    Return: list 
+    """
+    return dir(obj)
