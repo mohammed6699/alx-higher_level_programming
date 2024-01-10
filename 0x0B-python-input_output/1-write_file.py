603f216bccb7327282f618f6d14229c763bdaf7d
mohammed6699 <mohammedahmed.refaat@gmail.com>
2024-01-10 14:09:22 -0800

    ./1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Write and print the file content"""
+
+def write_file(filename="", text=""):
+    """write and read the file content"""
+    with open(filename, 'w',  encoding='utf-8') as file:
+        return (file.write(text))
