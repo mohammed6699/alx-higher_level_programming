5049a019a4c048d75c257b79a6c0c086178ceb70
root <mohammedahmed.refaat@gmail.com>
2024-02-14 17:58:54 -0800

    ./3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+    module to define the class'Rectangle'
+"""
+
+class Rectangle:
+    """defition for class Rectangle"""
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """getter for private instance width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter for private instance width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter for private instance height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """module to return area of Rectangle"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """module to return perimeter of rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        
+        if self.__width == 0 and self.__height ==0:
+            return ""
+        return ("\n".join("#" * self.__width for j in range (self.__height)))
