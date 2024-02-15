17834769c522704e708efbdaf97e548c78870aa0
root <mohammedahmed.refaat@gmail.com>
2024-02-14 18:49:07 -0800

    ./4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -54,4 +54,4 @@ class Rectangle:
 
     def __repr__(self):
         """return a string representation of the rectangle """
-        return "Rectangle({:d}, {:d}".format(self.__width, self.__height)
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
