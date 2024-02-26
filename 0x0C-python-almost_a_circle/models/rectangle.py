b8f308fa29cc64a871f3248637cf958dc5711bda
mohammed6699 <mohammedahmed.refaat@gmail.com>
2024-02-26 13:34:49 -0800

    ./10

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,38 +10,30 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
-
     @property
     def width(self):
         return self.__width
-
     @width.setter
     def width(self, value):
         self.validate_integer("width", value, False)
         self.__width = value
-
     @property
     def height(self):
         return self.__height
-
     @height.setter
     def height(self, value):
         self.validate_integer("height", value, False)
         self.__height = value
-
     @property
     def x(self):
         return self.__x
-
     @x.setter
     def x(self, value):
         self.validate_integer("x", value)
         self.__x = value
-
     @property
     def y(self):
         return self.__y
-    
     @y.setter
     def y(self, value):
         self.validate_integer("y", value)
@@ -73,16 +65,12 @@ class Rectangle(Base):
     def __update(self, id = None, width = None, height = None, x = None, y = None):
         if id is not None:
             self.id = id
-
         if width is not None:
             self.width = width
-
         if height is not None:
             self.height = height
-
         if x is not None:
             self.x = x
-
         if y is not None:
             self.y = y
 
