f02681f7e5c6d4fc1ef6dbf14847ddda5d9da45c
root <mohammedahmed.refaat@gmail.com>
2024-04-22 14:29:49 -0700

    ./10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function factorial(n){
+if(n === 0) {
+	return 1;
+} else {
+	return n * factorial(n - 1);
+}
+}
+
+const num = parseInt( process.argv[2]);
+const result = factorial(num);
+console.log(result);
