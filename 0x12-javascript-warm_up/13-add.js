93b9685b4a5f344ffe59c45a6c772f92f8304d4a
root <mohammedahmed.refaat@gmail.com>
2024-04-22 14:47:53 -0700

    ./13

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const add = (a, b)=> {
+	return(a + b);
+};
+module.exports = add;
+/*
+const var1 = parseInt(process.argv[2]);
+const var2 = parseInt(process.argv[3]);
+const rsult = add(var1, var2);
+const.log(rsult);
+*/
