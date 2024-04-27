fb473f233dce69ec0351b988293e62a4290eb308
root <mohammedahmed.refaat@gmail.com>
2024-04-27 12:11:55 -0700

    ./100

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- script that displays the average temperature
-SELECT `city`, AVG(`score`) AS `average`
+SELECT `city`, AVG(`score`) AS `avg_temp`
 FROM `temperatures`
 GROUP BY `city`
 ORDER BY `avg_temp` DESC;
