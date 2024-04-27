87f4d2c7812d60bb521431d09670acdd74e80e8a
root <mohammedahmed.refaat@gmail.com>
2024-04-27 02:07:12 -0700

    ./11

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- script that lists all records of the table second_table.
+-- script that lists all records with a score >= 10.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
+ORDER BY `score` DESC;
