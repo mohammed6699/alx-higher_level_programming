6229187191ad2f085ef8c74b53d25ae3cdc877d8
root <mohammedahmed.refaat@gmail.com>
2024-05-09 14:35:13 -0700

    ./0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the respose
 
-curl -s "$1" | wc -c
+if [ $# -ne 1 ]; then
+	exit 1
+fi
+
+url="0.0.0.0:5000"
+response=$(curl -s -o - "$url")
+size=$(echo "$response" | wc -c )
+echo "$size"
