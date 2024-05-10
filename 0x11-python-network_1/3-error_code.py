2d820ecc53df48fdff5697c977137df9f5cbe1ac
root <mohammedahmed.refaat@gmail.com>
2024-05-10 12:13:24 -0700

    ./3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" script that takes in a URL, sends a request to the URL and displays the body of the response"""
+
+import sys
+import urllib.request
+import urllib.error
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error Code: {}".format(e.code))
