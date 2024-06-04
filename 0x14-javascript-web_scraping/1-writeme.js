47edbcfd64804b1e37c551f4ae4223c3959c37a6
root <mohammedahmed.refaat@gmail.com>
2024-06-04 12:55:14 -0700

    ./0,1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -7,9 +7,9 @@
 // import fs module for node.js
 const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', error=> {
-        // process.argv[2] the file to write in
-        // process.argv[3] the sentence to write in the file
-        if (error){
+        if (error)
+	{
                 console.error(error);
         }
 });
+
