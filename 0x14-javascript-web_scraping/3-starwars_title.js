06206486d617dc08ed812cac3c99ea40c8644e44
root <mohammedahmed.refaat@gmail.com>
2024-06-04 17:04:55 -0700

    ./all

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
+/*
+ * script that prints the title of a Star Wars movie 
+ * the episode number matches a given integer.
+ */
+
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, function(error, response, body) {
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 
+/*
+ * script that prints the number of movies where,
+ * the character “Wedge Antilles” is present.
+ */
+
 const request = require('request');
 request(process.argv[2], function(error, response, body){
 	if (!error) {
